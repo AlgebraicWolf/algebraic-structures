7b85f60dfd2f5fe9a89d8dc45ca8acac5f504f99
Aleksei Volkov <volkov.aa@phystech.edu>
2021-03-18 18:03:23 +0300

    Implemented Monoid concept together with a Monoid function lifter

--- monoid.hpp
+++ monoid.hpp
@@ -9,4 +9,13 @@ template<typename T>
 concept Monoid = Semigroup<T> &&  // Monoid must be a semigroup...
                  HasIdentity<T>;  // And have an identity element (substituted by the function Unit -> Id)
 
+// As with semigroup, for a monoid M we can consider T -> M to be a monoid type as well. The identity element of such monoid is a function that maps all
+// values of type T to the identity element of the monoid
+
+template<typename T, Monoid M>
+class MonoidFunctionLift : public SemigroupFunctionLift<T, M> {
+    public:
+    MonoidFunctionLift(Unit) : SemigroupFunctionLift([](T) { return M(Unit()); }) {} // Add constructor for unit type that constructs id 
+}; // Now we can lift functions into monoids to monoids themselves
+
 #endif 
