4ce0c2357e75cf78e749ad51b4c2cc757e748022
Aleksei Volkov <volkov.aa@phystech.edu>
2021-03-18 20:42:22 +0300

    FizzBuzz now uses monoids for the sake of testing identity on function lifts

--- examples/FizzBuzz.cpp
+++ examples/FizzBuzz.cpp
@@ -1,18 +1,30 @@
-// Implementation of FizzBuzz via semigroups together with a wrapper around it
+// Implementation of FizzBuzz via monoids together with a wrapper around them
 // Allows to easily extend the functionality via adding new monoid elements
-#include "../semigroup.hpp"
+#include "../monoid.hpp"
 #include <string>
 #include <iostream>
 
-std::string operator*(std::string a, std::string b)
+// Wrap around std::string that adds identity element
+class StringWrap : public std::string
 {
-    if(a.length() && b.length()) return a + " " + b; // Operation on strings are concatenations that add a space between the two elements
-    else if (a.length()) return a;
-    else return b;
+public:
+    StringWrap(Unit) : std::string("") {}
+    StringWrap(const std::string &s) : std::string(s) {}
+    StringWrap(const char *s) : std::string(s) {}
+};
+
+StringWrap operator*(StringWrap a, StringWrap b)
+{
+    if (a.length() && b.length())
+        return a + StringWrap(" ") + b; // Operation on strings are concatenations that add a space between the two elements
+    else if (a.length())
+        return a;
+    else
+        return b;
 }
 
 // Function that passes number through if function combination acting upon it returned empty string
-std::string numberPassthrough(int x, SemigroupFunctionLift<int, std::string> f)
+StringWrap numberPassthrough(int x, MonoidFunctionLift<int, StringWrap> f)
 {
     auto result = f(x);
     if (result.length() != 0)
@@ -23,16 +35,18 @@ std::string numberPassthrough(int x, SemigroupFunctionLift<int, std::string> f)
 
 int main()
 {
-    SemigroupFunctionLift<int, std::string> f1([](int x) { return x % 3 == 0 ? "Fizz" : ""; });
-    SemigroupFunctionLift<int, std::string> f2([](int x) { return x % 5 == 0 ? "Buzz" : ""; });
+    MonoidFunctionLift<int, StringWrap> f1([](int x) { return x % 3 == 0 ? "Fizz" : ""; });
+    MonoidFunctionLift<int, StringWrap> f2([](int x) { return x % 5 == 0 ? "Buzz" : ""; });
+    MonoidFunctionLift<int, StringWrap> f3(Unit{}); // Unit element that does nothing for the purpose of testing 
 
-    auto f = f1 * f2;
+    auto f = f1 * f2 * f3;
 
     int N = 0;
     std::cout << "Iterate from 1 to ";
     std::cin >> N;
 
-    for(int i = 1; i <= N; i++) {
+    for (int i = 1; i <= N; i++)
+    {
         std::cout << numberPassthrough(i, f) << "\n";
     }
 }
